4def2c166deb94a6a88c8328cf4d64b64ede9b40
choiwinner <babyrain@naver.com>
2025-01-19 00:18:12 +0900

    :feet test

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ with st.echo():
 
     @st.cache_resource
     def get_driver():
+
+
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+
         return webdriver.Chrome(
             service=Service(
                 ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
@@ -28,9 +36,6 @@ with st.echo():
             options=options,
         )
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
     #options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,   #like Gecko) Chrome/58.0.3029.110 Safari/537.3')
 
     driver = get_driver()
